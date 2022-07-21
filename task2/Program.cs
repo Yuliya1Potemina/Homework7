569ff20604540d355d6a95fea101953468c1ade2
Yuliya <optik-yulya@mail.ru>
2022-07-21 13:26:46 +0300

    Зададча50

---
+++ task2/Program.cs
@@ -0,0 +1,69 @@
+﻿
+/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+int[,] InitMatrix(int m, int n)
+
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+
+       
+
+}
+
+int pos1=-1, pos2=-1; int[,] arr;
+{
+if 
+
+  
+ (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+            {
+                Console.WriteLine("Элемент не существует");
+            }
+            else
+            {
+                Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+            }
+            Console.ReadLine();
+}
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = InitMatrix(m, n);
+Console.WriteLine($"Матрица размером {m}x{n}:");
+Console.WriteLine();
